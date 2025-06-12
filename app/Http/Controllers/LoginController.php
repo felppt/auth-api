55a29d080380375adadb95aaeeb746582ad97611
root <root@LAPTOP-GBU9VEK7.localdomain>
2025-06-12 18:28:20 +0300

    2-ой способ входа

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,26 +3,52 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Login\StoreRequest;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
     public function store(StoreRequest $request)
     {
         $data = $request->only([
-            'email', 'password',
+            'email',
+            'password',
         ]);
         $remember = (bool) $request->input('remember');
 
-        if (!Auth::attempt($data, $remember)) {
+        // if (!Auth::attempt($data, $remember)) {
+        //     return back()->withErrors([
+        //         'email' => 'Неверный логин или пароль',
+        //     ])->onlyInput('email');
+
+        // }
+
+        // $request->session()->regenerate();
+
+        // return redirect()->to('login');
+
+        // 2 способ
+        $user = User::query()
+            ->where('email', $data['email'])
+            ->first();
+
+        if (!$user) {
             return back()->withErrors([
                 'email' => 'Неверный логин или пароль',
             ])->onlyInput('email');
-
         }
 
+        if (!Hash::check($data['password'], $user->password)) {
+            return back()->withErrors([
+                'email' => 'Неверный логин или пароль',
+            ])->onlyInput('email');
+        }
+
+        Auth::login($user, $remember);
+
         $request->session()->regenerate();
-        
+
         return redirect()->to('login');
     }
 }
